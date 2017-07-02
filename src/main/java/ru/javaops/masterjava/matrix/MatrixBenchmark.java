ce68353d0bfa1a4fff769aefa1c9e00bbbb0f6c9
petrovale <leha.isakov@yandex.ru>
2017-07-02 22:21:21 +0300

    2_05_JMH_main_jar

--- src/main/java/ru/javaops/masterjava/matrix/MatrixBenchmark.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixBenchmark.java
@@ -1,6 +1,11 @@
 package ru.javaops.masterjava.matrix;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,11 +21,11 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 @Threads(1)
-@Fork(1)
+@Fork(10)
 @Timeout(time = 5, timeUnit = TimeUnit.MINUTES)
 public class MatrixBenchmark {
     // Matrix size
-    @Param({"100", "1000"})
+    @Param({"1000"})
     private int matrixSize;
 
     private static final int THREAD_NUMBER = 10;
@@ -35,12 +40,22 @@ public class MatrixBenchmark {
         matrixB = MatrixUtil.create(matrixSize);
     }
 
-    @Benchmark
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(MatrixBenchmark.class.getSimpleName())
+                .threads(1)
+                .forks(10)
+                .timeout(TimeValue.minutes(5))
+                .build();
+        new Runner(options).run();
+    }
+
+//    @Benchmark
     public int[][] singleThreadMultiplyOpt() throws Exception {
         return MatrixUtil.singleThreadMultiplyOpt(matrixA, matrixB);
     }
 
-    @Benchmark
+//    @Benchmark
     public int[][] concurrentMultiplyStreams() throws Exception {
         return MatrixUtil.concurrentMultiplyStreams(matrixA, matrixB, executor);
     }
