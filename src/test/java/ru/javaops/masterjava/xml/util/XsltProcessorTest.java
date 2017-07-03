f3b3bdda623b2fdbc7ae0bfbd16488f8b4345a66
petrovale <leha.isakov@yandex.ru>
2017-07-03 17:38:59 +0300

    2_10_Xslt

---
+++ src/test/java/ru/javaops/masterjava/xml/util/XsltProcessorTest.java
@@ -0,0 +1,22 @@
+package ru.javaops.masterjava.xml.util;
+
+import com.google.common.io.Resources;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * gkislin
+ * 23.09.2016
+ */
+public class XsltProcessorTest {
+    @Test
+    public void transform() throws Exception {
+        try (InputStream xslInputStream = Resources.getResource("cities.xsl").openStream();
+             InputStream xmlInputStream = Resources.getResource("payload.xml").openStream()) {
+
+            XsltProcessor processor = new XsltProcessor(xslInputStream);
+            System.out.println(processor.transform(xmlInputStream));
+        }
+    }
+}
