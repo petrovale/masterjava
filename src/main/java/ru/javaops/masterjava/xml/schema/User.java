b5ea6c59e2fa91062182a6411ec2677328710593
petrovale <leha.isakov@yandex.ru>
2017-07-08 12:56:53 +0300

    3_2_HW2_JAXB_HTML

--- src/main/java/ru/javaops/masterjava/xml/schema/User.java
+++ src/main/java/ru/javaops/masterjava/xml/schema/User.java
@@ -1,16 +1,9 @@
 
 package ru.javaops.masterjava.xml.schema;
 
+import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlIDREF;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.XmlValue;
 
 
 /**
@@ -179,5 +172,11 @@ public class User {
         }
         return this.groupRefs;
     }
+
+    @Override
+    public String toString() {
+        return value + '(' + email + ')';
+    }
+
 
 }
