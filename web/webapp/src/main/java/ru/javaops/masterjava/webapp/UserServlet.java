5acfbfcc6f019987b83961accde1ff7329ef884b
petrovale <leha.isakov@yandex.ru>
2017-07-18 21:28:18 +0300

    HW4_2_first 20 users in the webapp

---
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UserServlet.java
@@ -0,0 +1,31 @@
+package ru.javaops.masterjava.webapp;
+
+import org.thymeleaf.context.WebContext;
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.persist.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static ru.javaops.masterjava.webapp.ThymeleafListener.engine;
+
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+
+        UserDao dao = DBIProvider.getDao(UserDao.class);
+        List<User> users = dao.getWithLimit(20);
+
+        webContext.setVariable("users", users);
+        engine.process("webapp", webContext, resp.getWriter());
+    }
+}
